03331d365b4980f16b0b50a38f1e8ec066233b00
RT <aarti.bhardwaj@twinspark.co>
2017-07-04 18:24:37 +0530

    updated composer.json

---
+++ InspiniaTheme/config/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\Event\EventManager;
+use Cake\Log\Log;
+use Cake\Routing\DispatcherFactory;
+
